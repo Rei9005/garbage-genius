da5357e537338e421ab9d599e64e1516d6f40fe4
Junhyeok Yoon <junhyeokyoon3@gmail.com>
2024-05-23 15:14:01 +0900

    edituser

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,29 @@
 class UsersController < ApplicationController
   # before_action :set_user, only: [:edit, :update]
 
-  # def edit
-  #   @user
-  # end
+  def edit
+     @user
+   end
 
 
-  # def update
-  #   if @user.update(user_params)
-  #     redirect_to edit_user_path(@user), notice: 'Profile was successfully updated.'
-  #   else
-  #     render :edit
-  #   end
-  # end
+   def update
+     if current_user.update(user_params)
+       redirect_to dash_path, notice: 'Profile was successfully updated.'
+     else
+       render :edit, status: :uprocessable_entity
+     end
+   end
 
-  # private
+   private
 
-  # def set_user
-  #   @user = current_user
-  # end
+   def set_user
+     @user = current_user
+   end
 
-  # def user_params
-  #   params.require(:user).permit(:email, :password, :password_confirmation, :name, :address, :morning_alert, :evening_alert)
-  # end
-# end
+   def user_params
+     params.require(:user).permit(:email, :name, :address, :morning_alert, :evening_alert)
+   end
+ end
 
 
 
@@ -43,8 +43,4 @@ class UsersController < ApplicationController
     else
       render 'spots', status: :uprocessable_entity
     end
-
-  end
-
-
 end
