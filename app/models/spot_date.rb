daf5c0d7e887dde23b4b98e7209d3832448aa266
Alex Lee <lee.a.esq@gmail.com>
2024-05-28 14:17:58 +0900

    ETA seed is work in progress

--- app/models/spot_date.rb
+++ app/models/spot_date.rb
@@ -4,4 +4,21 @@ class SpotDate < ApplicationRecord
 
   validates :scheduled_time, presence: true
 
+
+
+  def average_pickup_time
+    # get all the spot_dates for a specific day of the week
+    days = spot.spot_dates.where("extract(dow from date) = ?", date.wday)
+    # get all the times that people registered for those days
+    times = SpotVisit.where(spot_date: days).pluck(:time)
+    # returning the normal scheduled time if no one has registered??
+    return scheduled_time if times.empty?
+    # get the average of timestamps
+    average_time = times.map(&:to_i).sum / times.count
+    # convert back into time
+    Time.at(average_time)
+  end
+
+
 end
+
